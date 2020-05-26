0f282b7b0512ced65052bac16b358e1a3b6f323a
edycakra <edycakrasinaga@gmail.com>
2020-05-26 09:36:01 +0700

    adding endpoint inventory and its dummy data

--- main.go
+++ main.go
@@ -22,12 +22,34 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Endpoint homePage is called")
 }
 
+func getInventory(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Println("Calling getInventory")
+	json.NewEncoder(w).Encode(inventory)
+}
+
 func handleRequests() {
 	router := mux.NewRouter().StrictSlash(true)
+
+	//list of routes
 	router.HandleFunc("/", homePage).Methods("GET")
+	router.HandleFunc("/inventory", getInventory).Methods("GET")
+
 	log.Fatal(http.ListenAndServe(":3000", router))
 }
 
 func main() {
+	inventory = append(inventory, Item{
+		UID: "0",
+		Name: "Cheese",
+		Desc: "Pasta Filata",
+		Price: 4.99,
+	})
+	inventory = append(inventory, Item{
+		UID: "1",
+		Name: "Milk",
+		Desc: "Lowfat Milk",
+		Price: 3.25,
+	})
 	handleRequests()
 }
