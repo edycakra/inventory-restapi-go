a841b9c34bfac1cc9276504e265778c764de60ce
edycakra <edycakrasinaga@gmail.com>
2020-05-26 09:14:54 +0700

    adding Item

--- main.go
+++ main.go
@@ -4,8 +4,23 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"encoding/json"
 	"github.com/gorilla/mux"
 )
+
+//Item for inventories
+type Item struct { 
+	UID string `json:"UID"`
+	Name string `json:"Name"` 
+	Desc string `json:"Desc"`
+	Price float64 `json:"Price"`
+}
+
+var inventory []Item
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Endpoint homePage is called")
+}
 
 func handleRequests() {
 	router := mux.NewRouter().StrictSlash(true)
@@ -13,4 +28,6 @@ func handleRequests() {
 	log.Fatal(http.ListenAndServe(":3000", router))
 }
 
-func main() {}
+func main() {
+	handleRequests()
+}
